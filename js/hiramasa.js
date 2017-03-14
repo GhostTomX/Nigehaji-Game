2357697fa19f1d0d9b3cbb14520b7d3a485e73cc
GhostTomX <r04522839@ntu.edu.tw>
2017-03-14 21:44:11 +0800

    精确的家里结构的位置限制
    感觉是个体力活。。。快点做争取本周做完吧

--- js/hiramasa.js
+++ js/hiramasa.js
@@ -2,15 +2,14 @@
      var name = "this windows";
      var hi = new Person("#hiramasa");
      var mi = new Person("#mikuri");
+     personMoveControl(hi);
      console.log(hi.position);
      console.log(mi.position);
-     personMoveControl(hi);
 
      function personMoveControl(obj) {
          $(document).keydown(function (event) {
              obj.positionValueTest();
-             console.log(obj.position);
-             console.log(obj.position_top);
+             console.log(obj.position.top, obj.position.left);
              switch (event.keyCode) {
              case 40: // down
                  if (obj.position_down) {
@@ -60,7 +59,8 @@
              this.position_down = true;
              this.position_left = true;
              this.position_right = true;
-             if (this.position.top <= 8) {
+             //contious range ,so can't use case
+             if (this.position.top <= 8) { //Outside limitation
                  this.position_top = false;
              }
              if (this.position.left <= 8) {
@@ -72,6 +72,36 @@
              if (this.position.left > 968) {
                  this.position_right = false;
              }
+             //House structure limitation according to position.left
+             if ((this.position.left === 68 & this.position.top < 308) ||
+                     (this.position.left === 68 & this.position.top > 508) ||
+                 (this.position.left === 308 & this.position.top < 248))
+              { //  ベランダ    
+                 this.position_right = false;
+             }
+
+
+             if ((this.position.left === 88 & this.position.top === 308) ||
+                 ((this.position.left >= 108 & this.position.top === 268) &
+                     (this.position.left <= 208 & this.position.top === 268))
+             ) {
+                 this.position_top = false;
+             }
+
+
+             if (this.position.left === 88 & this.position.top === 508) { //  ベランダ ドア   
+                 this.position_down = false;
+             }
+
+
+             if ((this.position.left === 108 & this.position.top < 308) ||
+                 (this.position.left === 108 & this.position.top > 508)) { //  ベランダ    
+                 this.position_left = false;
+             }
+
+
+
+
          };
      };
  });
